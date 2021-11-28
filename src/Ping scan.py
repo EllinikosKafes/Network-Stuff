64aeaccfc9ad24f7f9c6e14a17c28bc259aa053d
RysteQ <34135554+RysteQ@users.noreply.github.com>
2021-11-28 05:47:26 +0200

    Update Ping scan.py
    Cleaned it up a bit more, although I expect to clean it up even more in the following hours / days

--- src/Ping scan.py
+++ src/Ping scan.py
@@ -2,9 +2,7 @@
 # Currently it only supports for a scan of max 256 hosts (/24 or 0-255) for ease and faster execution time (Warning : it's slow).
 # It is currently supported only for Windows , some modifications have to happen to be supported in other OS.
 
-import subprocess
-import threading
-import time
+import subprocess, threading, time
 
 
 # This function takes the initial string (input) and discards all the spaces , so we can make our job easier and more predictable!
@@ -22,6 +20,7 @@ def noSpaceString(ip_range):
 # Save each index to index_list and return its elements. We execute this function in the get_octets function .
 def get_indexes(ip_range, method):
     index_list = []
+    
     for i in range(len(ip_range)):
         if ip_range[i] == ".":
             index_list.append(i)
@@ -40,7 +39,6 @@ def get_indexes(ip_range, method):
 # convert all the values into integers we return it. I used several string methods to isolate the octets from the
 # dots '.' and slash '/' or dash '-' (depending on the method that the user chooses). I also used the get_indexes (
 # ip_range) function, which aims to give me the indexes of the dots, so that I can "find" the digits between the dots.
-
 def get_octets(ip_range, method):
     octets = []
     ind1, ind2, ind3, index_of_slash_or_dash = get_indexes(ip_range, method)
@@ -57,7 +55,6 @@ def get_octets(ip_range, method):
 
 # In this function , we get the prefix length (e.g. /24) or the end address , which is the last octet that the user
 # has put (e.g. -245) , depending on the method.
-
 def get_prefix_or_end_address(ip_string, method):
     length = len(ip_string)
     
@@ -74,7 +71,6 @@ def get_prefix_or_end_address(ip_string, method):
 # less than 24 and if the general character population is more or less than we expect. For now, I have not programmed
 # it to support prefixes less than 24 for reasons of simplicity and convenience (because we need to check and process
 # only the last octet).
-
 def check_for_errors(ip_range, speed):
     valid_values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '/', '.', '-']
     prefix = -1
@@ -93,7 +89,6 @@ def check_for_errors(ip_range, speed):
 
 
     for j in range(len(ip_range)):
-
         if ip_range[j] not in valid_values:
             print("Not valid character found ('", ip_range[j], "') , Please try again ....\n")
             return False
@@ -101,8 +96,7 @@ def check_for_errors(ip_range, speed):
     if len(ip_range) > 18 or len(ip_range) < 9:
         print("More characters typed than expected , Please try again ....\n")
         return False
-
-    if '-' in ip_range and '/' in ip_range:
+    elif '-' in ip_range and '/' in ip_range:
         print("Not supported , Please try again ....")
         return False
 
@@ -116,7 +110,7 @@ def check_for_errors(ip_range, speed):
         return False
 
     # Exporting the 4 octets as integers in a list.
-    octets = get_octets(ip_range , method )
+    octets = get_octets(ip_range, method)
 
 
     if method == '1':
@@ -124,7 +118,7 @@ def check_for_errors(ip_range, speed):
     else:
         end_address = get_prefix_or_end_address(ip_range, method)
 
-    for j in range(0, 4):
+    for j in range(4):
         if octets[j] > 255 or octets[j] < 0:
             print("The input value of an octet is exceeding the limits of an IPv4 address , Please try again ....\n")
             return False
@@ -146,25 +140,19 @@ def check_for_errors(ip_range, speed):
     return method , octets , prefix , end_address
 
 
-
-
 def init_process():
-    ip_range = str(
-        input("Type the IPv4 address range that you wish to scan following these two methods : ( 192.168.1.0/24 )   "
-              "or   ( 192.168.1.0-255 )    :"))
+    ip_range = input("Type the IPv4 address range that you wish to scan following these two methods : ( 192.168.1.0/24 )   "
+              "or   ( 192.168.1.0-255 )    :")
 
     # We remove all the spaces added by the user by mistake, in order to make the job easier (because of the indexes)
     # and more predictable.
     ip_range = noSpaceString(ip_range)
-
-    speed = input("Choose speed ( 1 , 2 , 3 ) :")
-
-
+    speed = input("Choose speed ( 1 , 2 , 3 ):")
 
     return ip_range, speed
 
 
-# This the fuction that we ping from our system (Only for Windows10). After the ping, we check the returncode and its
+# This the fuction that we ping from our system (Only for Windows). After the ping, we check the returncode and its
 # message to find out what the result was . Returncode 0 means that a command was executed successfully, but for some
 # reason in Windows the messages "Destination host unreachable", "Destination net unreachable" and a successful echo
 # reply, have all returncode 0 (unlike linux). For this reason , I also checked their output(stdout). The addresses
@@ -174,36 +162,29 @@ def scanning_process(first_address, last_address ):
 
     for j in range(first_address, last_address):
 
-        # if we exceed the limits of the last octet , abort.
+        # fail conditions
         if j > 255:
             break
-        # if we pass the end address , abort.
         elif method == '2' and j > end_address:
             break
 
-        # The current address to be scanned.
+        # Check active hosts with the ping command
         address = str(octets[0]) + "." + str(octets[1]) + "." + str(octets[2]) + "." + str(j)
-
-        # This is the actual command that runs on the background for all the ip addresses (ping "192.168.1.1" -n 3)
         res = subprocess.run(["ping", address, "-n", '3', ], capture_output=True, text=True)
 
         if res.returncode == 0 and (res.stdout.count("Destination host unreachable")) > 1:
             print(address, "is unreachable!")
-
         elif res.returncode == 0 and (res.stdout.count("Destination net unreachable")) > 1:
             print("The network of ", address, "is unreachable!")
-
         elif res.returncode == 0:
             print(address, "is responsive!")
             active_ips.append(address)
-
         else:
             print("Ping to " + address, "failed!")
 
 
 # Function to sort the list of active hosts from the active_ips list.
 # Some processes end faster than their predecessors, so we need to sort the list. This function is optional.
-
 def bubble_sort(active_ips):
     last_octets = []
     # In this loop we append every last octet of the active IPv4 adresses in the last_octets list.
@@ -216,16 +197,11 @@ def bubble_sort(active_ips):
         last_octets.append(int(oct4))
 
     for i in range(len(active_ips)):
-
         for j in range(0, len(active_ips) - i - 1):
-
             if last_octets[j] > last_octets[j + 1]:
-                holder_variable = last_octets[j]
-                last_octets[j] = last_octets[j + 1]
-                last_octets[j + 1] = holder_variable
-                holder_variable = active_ips[j]
-                active_ips[j] = active_ips[j + 1]
-                active_ips[j + 1] = holder_variable
+                last_octets[j], last_octets[j + 1] = last_octets[j + 1], last_octets[j]
+                active_ips[j], active_ips[j + 1] = active_ips[j + 1], active_ips[j]                
+
     return active_ips
 
 
@@ -251,7 +227,6 @@ method , octets , prefix , end_address = check_for_errors(ip_range , speed)
 start_time = time.time()
 
 if method == '1':
-
     if octets[3] + pow(2, 32 - prefix) > 256:
         count = 256 - octets[3]
     else:
@@ -259,7 +234,6 @@ if method == '1':
 
 
 elif method == '2':
-
     if end_address > octets[3]:
         count = end_address - octets[3] + 1
     elif end_address < octets[3]:
@@ -303,18 +277,8 @@ start = octets[3]
 finish = start + increment
 
 
-
 if int(speed) == 1:
     if count < 8:
-        for i in range(count):
-            t = threading.Thread(target=scanning_process, args=(start, finish,))
-            t.start()
-            threads.append(t)
-
-            start = finish
-            finish += increment
-
-    else:
         for i in range(8):
             t = threading.Thread(target=scanning_process, args=(start, finish,))
             t.start()
@@ -324,15 +288,6 @@ if int(speed) == 1:
             finish += increment
             
 elif int(speed) == 2:
-    if count < 16:
-        for i in range(count):
-            t = threading.Thread(target=scanning_process, args=(start, finish,))
-            t.start()
-            threads.append(t)
-
-            start = finish
-            finish += increment
-    else:
         for i in range(16):
             t = threading.Thread(target=scanning_process, args=(start, finish,))
             t.start()
@@ -342,15 +297,6 @@ elif int(speed) == 2:
             finish += increment
             
 else:
-    if count < 32:
-        for i in range(count):
-            t = threading.Thread(target=scanning_process, args=(start, finish,))
-            t.start()
-            threads.append(t)
-
-            start = finish
-            finish += increment
-    else:
         for i in range(32):
             t = threading.Thread(target=scanning_process, args=(start, finish,))
             t.start()
