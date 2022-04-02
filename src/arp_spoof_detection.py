40f51e56ccd46bb96e8d371dec05689a66f2d170
RysteQ <34135554+RysteQ@users.noreply.github.com>
2022-04-02 08:06:46 -0700

    Removed useless lines
    I removed some lines due to them being useless or simply because they were a bad practice.

--- src/arp_spoof_detection.py
+++ src/arp_spoof_detection.py
@@ -1,5 +1,4 @@
-import subprocess
-import re
+import subprocess, re
 from time import sleep
 import scapy.all as scapy
 
@@ -21,49 +20,44 @@ def determine_gatewayipandmac():
 
 
 def scanning_spoofed_mac(gateway_ip, gateway_mac):
-    global attacker
     res3 = subprocess.run(["arp", "-a"], capture_output=True, text=True, shell=True)
     search_for_duplicates = re.findall(f".*{gateway_mac}", res3.stdout)
+    attacker = ""
 
     # If we find more than one ARP entry , it means that someone is "pretending" to be the gateway/router .
     if len(search_for_duplicates) > 1:
         for i in range(len(search_for_duplicates)):
             x2 = re.search("([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})", search_for_duplicates[i])
             temp_ip = search_for_duplicates[i][x2.span()[0]: x2.span()[1]]
+            
             if temp_ip != gateway_ip:
                 attacker = temp_ip
-        return True
-    else:
-        return False
 
-
-def arp_spoof_prevention(request, reply):
-    scapy.send(reply ,verbose = False)
-    received = scapy.sr1(request , verbose = False)
-    print("Gateway's TRUE MAC : ",received.getfieldval('hwsrc'))
+        return attacker, True
+    
+    return attacker, False
 
 
 gateway_ip, gateway_mac = determine_gatewayipandmac()
-
 request = scapy.ARP(op=1, pdst=gateway_ip, psrc="192.168.1.30", hwdst=scapy.getmacbyip(gateway_ip))
 reply = scapy.ARP(op=2, pdst=gateway_ip, psrc="192.168.1.30", hwdst=scapy.getmacbyip(gateway_ip))
-attacker = ''
 
 while True:
     try:
-        gateway_ip, gateway_mac = determine_gatewayipandmac()
         print(f"IP of gateway is : {gateway_ip} \nMAC of gateway is : {gateway_mac}")
-        # arp_spoof_prevention(request, reply)
+        gateway_ip, gateway_mac = determine_gatewayipandmac()
+        attacker, spoofingCheck = scanning_spoofed_mac(gateway_ip, gateway_mac)
 
-        if scanning_spoofed_mac(gateway_ip, gateway_mac):
+        if spoofingCheck:
+            scapy.send(reply, verbose = False)
+
             print(f"Warning ! {attacker} is spoofing his MAC to the default gateway !\nStarting ARP spoof prevention....")
-            arp_spoof_prevention(request, reply)
+            print("Gateway's TRUE MAC : ", scapy.sr1(request, verbose = False).getfieldval('hwsrc'))
         else:
             print("All good !")
 
         sleep(5)
 
     except KeyboardInterrupt:
-
         print("Stopping the checks.....")
         break
